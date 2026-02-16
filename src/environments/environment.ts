127e3a162b2964213df98c09b4eb871b955f04c6
AyaaMostafa <aya.mostafa@remain.com.sa>
2026-02-16 16:20:57 +0200

    .,.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     apiUrl: 'https://localhost:7191/api',
+    
     appName: 'Code Generator',
     version: '1.0.0',
     defaultLanguage: 'en',
